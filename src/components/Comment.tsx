4e5e7e198f5fe525a45e731363425b4a4bc93918
Matheus Costa <matheuscastiglia31@gmail.com>
2022-09-29 10:49:11 -0300

    Projeto concluído

---
+++ src/components/Comment.tsx
@@ -0,0 +1,61 @@
+import { ThumbsUp, Trash } from "phosphor-react";
+import { useState } from "react";
+import { Avatar } from "./Avatar";
+import styles from "./Comment.module.css";
+
+interface CommentProps {
+  content: string;
+  onDeleteComment: (comment: string) => void;
+}
+
+export function Comment({ content, onDeleteComment }: CommentProps) {
+  const [likeCount, setLikeCount] = useState(0);
+
+  function handleDeleteComment() {
+    onDeleteComment(content);
+  }
+
+  function handleLikeComment() {
+    setLikeCount((state) => {
+      return state + 1;
+    });
+  }
+
+  return (
+    <div className={styles.comment}>
+      <Avatar
+        src="https://github.com/EvertonWolff.png"
+        hasBorder={false}
+        alt="Profile"
+      />
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Matheus Costa</strong>
+              <time
+                title="14 de setembro às 16:07"
+                dateTime="2022-09-14 16:07:00"
+              >
+                Cerca de 1h atrás
+              </time>
+            </div>
+
+            <button onClick={handleDeleteComment} title="Deletar comentário">
+              <Trash size={20} />
+            </button>
+          </header>
+
+          <p>{content}</p>
+        </div>
+
+        <footer>
+          <button onClick={handleLikeComment}>
+            <ThumbsUp />
+            Aplaudir <span>{likeCount}</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,27 @@
+import { PencilLine } from "phosphor-react";
+import { Avatar } from "./Avatar";
+import styles from "./Sidebar.module.css";
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        src="https://images.unsplash.com/photo-1663094802300-3085addd0e98?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
+        className={styles.cover}
+      />
+
+      <div className={styles.profile}>
+        <Avatar src="https://avatars.githubusercontent.com/u/63810194?v=4" />
+        <strong>Matheus Costa</strong>
+        <span>Web Developer</span>
+      </div>
+
+      <footer>
+        <a href="#">
+          <PencilLine size={20} />
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  );
+}
